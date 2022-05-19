aae2fdcbddb34de08c7c517ad936cef2fd430981
Marcos Lauder Correia de Oliveira <marcoslauder@maringa.gov.br>
2022-05-19 16:55:40 -0300

    teste

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-# from chatterbot import ChatBot
+from chatterbot import ChatBot
 # from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
