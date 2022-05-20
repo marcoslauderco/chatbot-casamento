2a6853e1b4872b358116644037cfa02cbca40379
Marcos Lauder Correia de Oliveira <marcoslauder@maringa.gov.br>
2022-05-20 15:34:32 -0300

    read only

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ CORS(app)
 chatbot = ChatBot("Amazing Bot",
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     database_uri="postgres://cehwxqvmrxdguy:c23e4da69da5277379a2d705b0cf5018471c362d767ae90cffe73e0309f643f7@ec2-3-224-125-117.compute-1.amazonaws.com:5432/dbru51gsii5r3a",
-    read_only=False)
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train("./corpus/casamento.yml")
-trainer.train("./corpus/compliment.yml")
-trainer.train("./corpus/conversations.yml")
-trainer.train("./corpus/greetings.yml")
+    read_only=True)
+# trainer = ChatterBotCorpusTrainer(chatbot)
+# trainer.train("./corpus/casamento.yml")
+# trainer.train("./corpus/compliment.yml")
+# trainer.train("./corpus/conversations.yml")
+# trainer.train("./corpus/greetings.yml")
 
 @app.route("/")
 def home():
