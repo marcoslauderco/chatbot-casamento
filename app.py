a046abe04493c25efe4b9d96db86494cb6975224
Marcos Lauder Correia de Oliveira <marcoslauder@maringa.gov.br>
2022-05-20 10:52:00 -0300

    aprendendo novas conversas

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 
 chatbot = ChatBot("Amazing Bot",
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    database_uri="postgres://cehwxqvmrxdguy:c23e4da69da5277379a2d705b0cf5018471c362d767ae90cffe73e0309f643f7@ec2-3-224-125-117.compute-1.amazonaws.com:5432/dbru51gsii5r3a")
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train("./corpus/casamento.yml")
-trainer.train("./corpus/compliment.yml")
-trainer.train("./corpus/conversations.yml")
-trainer.train("./corpus/greetings.yml")
+    database_uri="postgres://cehwxqvmrxdguy:c23e4da69da5277379a2d705b0cf5018471c362d767ae90cffe73e0309f643f7@ec2-3-224-125-117.compute-1.amazonaws.com:5432/dbru51gsii5r3a",
+    read_only=True)
+# trainer = ChatterBotCorpusTrainer(chatbot)
+# trainer.train("./corpus/casamento.yml")
+# trainer.train("./corpus/compliment.yml")
+# trainer.train("./corpus/conversations.yml")
+# trainer.train("./corpus/greetings.yml")
 
 @app.route("/")
 def home():
@@ -20,8 +21,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    response = chatbot.generate_response(userText)
-    return str(response.text)
+    return str(chatbot.get_response(userText))
 
 
 if __name__ == "__main__":
