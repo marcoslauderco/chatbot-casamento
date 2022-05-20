bbb756d6303cf3df1c2dcddeb488bc56ed5093fd
Marcos Lauder Correia de Oliveira <marcoslauder@maringa.gov.br>
2022-05-20 10:41:08 -0300

    aprendendo novas conversas

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ app = Flask(__name__)
 
 chatbot = ChatBot("Amazing Bot",
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    database_uri="postgres://svuhlvfubmlwoa:33e69316cac0b35f5bb9f4844d9304d841da64250709015c12da7ad3142c4520@ec2-52-86-115-245.compute-1.amazonaws.com:5432/d4756h8ddhd1fs")
+    database_uri="postgres://cehwxqvmrxdguy:c23e4da69da5277379a2d705b0cf5018471c362d767ae90cffe73e0309f643f7@ec2-3-224-125-117.compute-1.amazonaws.com:5432/dbru51gsii5r3a")
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("./corpus/casamento.yml")
 trainer.train("./corpus/compliment.yml")
 trainer.train("./corpus/conversations.yml")
-trainer.train("./corpus/games.yml")
 trainer.train("./corpus/greetings.yml")
 
 @app.route("/")
@@ -21,7 +20,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
+    return str(chatbot.generate_response(userText).text)
 
 
 if __name__ == "__main__":
