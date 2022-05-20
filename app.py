88cc50875c90e133abc79fce6e422e8d0343aa33
Marcos Lauder Correia de Oliveira <marcoslauder@maringa.gov.br>
2022-05-20 10:06:39 -0300

    não aprender conversando

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ chatbot = ChatBot("Amazing Bot",
     database_uri="postgres://svuhlvfubmlwoa:33e69316cac0b35f5bb9f4844d9304d841da64250709015c12da7ad3142c4520@ec2-52-86-115-245.compute-1.amazonaws.com:5432/d4756h8ddhd1fs")
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("./corpus/casamento.yml")
+trainer.train("./corpus/compliment.yml")
+trainer.train("./corpus/conversations.yml")
+trainer.train("./corpus/games.yml")
+trainer.train("./corpus/greetings.yml")
+trainer.train("./corpus/money.yml")
 
 @app.route("/")
 def home():
@@ -17,7 +22,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(chatbot.generate_response(userText))
+    return str(chatbot.get_response(userText))
 
 
 if __name__ == "__main__":
