4306a3b3a2bf3eee7743682c67d7b2943483977e
Marcos Lauder Correia de Oliveira <marcoslauder@maringa.gov.br>
2022-05-23 10:32:52 -0300

    melhorando treinamento

--- app.py
+++ app.py
@@ -11,12 +11,6 @@ chatbot = ChatBot("Amazing Bot",
     database_uri="postgres://cehwxqvmrxdguy:c23e4da69da5277379a2d705b0cf5018471c362d767ae90cffe73e0309f643f7@ec2-3-224-125-117.compute-1.amazonaws.com:5432/dbru51gsii5r3a",
     read_only=True)
     
-# trainer = ChatterBotCorpusTrainer(chatbot)
-# trainer.train("./corpus/casamento.yml")
-# trainer.train("./corpus/compliment.yml")
-# trainer.train("./corpus/conversations.yml")
-# trainer.train("./corpus/greetings.yml")
-
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -25,6 +19,19 @@ def home():
 def get_bot_response():
     userText = request.args.get('msg')
     return str(chatbot.get_response(userText))
+
+@app.route("/traine")
+def get_bot_traine():
+    chatbotTraine = ChatBot("Amazing Bot",
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri="postgres://cehwxqvmrxdguy:c23e4da69da5277379a2d705b0cf5018471c362d767ae90cffe73e0309f643f7@ec2-3-224-125-117.compute-1.amazonaws.com:5432/dbru51gsii5r3a",
+    read_only=False)
+    trainer = ChatterBotCorpusTrainer(chatbotTraine)
+    trainer.train("./corpus/casamento.yml")
+    trainer.train("./corpus/compliment.yml")
+    trainer.train("./corpus/conversations.yml")
+    trainer.train("./corpus/greetings.yml")
+    return str("Chatbot treinado")
 
 
 if __name__ == "__main__":
